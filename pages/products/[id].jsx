5cc2aa12e6e1e6918a28dc8c6a4c34a02d0c8dba
AtheerAPeter <atheer07713991785@gmail.com>
2021-07-23 00:46:51 +0300

    initial

--- pages/products/[id].jsx
+++ pages/products/[id].jsx
@@ -11,37 +11,22 @@ const SingleProduct = ({ data }) => {
 
 export default SingleProduct;
 
-export async function getStaticProps({ params }) {
-  const res = await fetch(
-    `https://prisma-shop.herokuapp.com/v1/product/${params.id}`
-  );
-  const posts = await res.json();
-
-  return {
-    props: {
-      posts,
-    },
-    // Next.js will attempt to re-generate the page:
-    // - When a request comes in
-    // - At most once every 10 seconds
-    revalidate: 10, // In seconds
-  };
-}
-
-// This function gets called at build time on server-side.
-// It may be called again, on a serverless function, if
-// the path has not been generated.
-export async function getStaticPaths() {
-  const res = await fetch(`https://prisma-shop.herokuapp.com/v1/productAll`, {
+export async function getServerSideProps(context) {
+  const { id } = context.query;
+  var requestOptions = {
     method: "GET",
     redirect: "follow",
-  });
+  };
 
-  const posts = await res.json();
-
-  const paths = posts.data.map((post) => ({
-    params: { id: post.id.toString() },
-  }));
-
-  return { paths, fallback: "blocking" };
+  return fetch(
+    `https://prisma-shop.herokuapp.com/v1/product/${id}`,
+    requestOptions
+  )
+    .then((response) => response.json())
+    .then((result) => {
+      return {
+        props: { data: result.data }, // will be passed to the page component as props
+      };
+    })
+    .catch((error) => console.log("error", error));
 }
