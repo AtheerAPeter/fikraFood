fc8d4c72baa2c3f613556bfbd1bfc34429704705
AtheerAPeter <atheer07713991785@gmail.com>
2021-07-25 20:03:04 +0300

    pwa

--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,39 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
-class MyDocument extends Document {
+const APP_NAME = "Fikra Food";
+const APP_DESCRIPTION = "This app is for ordering food";
+
+export default class extends Document {
   static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
+    return await Document.getInitialProps(ctx);
   }
 
   render() {
     return (
-      <Html>
-        <Head />
+      <Html lang="en" dir="ltr">
+        <Head>
+          <meta name="application-name" content={APP_NAME} />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="default"
+          />
+          <meta name="apple-mobile-web-app-title" content={APP_NAME} />
+          <meta name="description" content={APP_DESCRIPTION} />
+          <meta name="format-detection" content="telephone=no" />
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta name="theme-color" content="#FFFFFF" />
+          {/* TIP: set viewport head meta tag in _app.js, otherwise it will show a warning */}
+          {/* <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover' /> */}
+
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/icons/apple-touch-icon.png"
+          />
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="shortcut icon" href="/icons/favicon.ico" />
+        </Head>
         <body>
           <Main />
           <NextScript />
@@ -18,5 +42,3 @@ class MyDocument extends Document {
     );
   }
 }
-
-export default MyDocument;
